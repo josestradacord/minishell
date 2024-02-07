fa25c93b6807be34087507657378d31df063bb78
Jose Estrada Cordero <jose80malaga@gmail.com>
2024-02-07 20:35:48 +0100

    Makefile fixed
    Now Makefile accepts rl_clear_history()

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: joestrad <joestrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 18:48:57 by joestrad          #+#    #+#             */
-/*   Updated: 2024/02/05 18:48:59 by joestrad         ###   ########.fr       */
+/*   Updated: 2024/02/07 20:08:34 by joestrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,35 @@ void	ft_new_cmd(t_ms *ms, int s, int e)
 	}
 }
 
+void	ft_new_tkn(t_ms *ms, int s, int e)
+{
+	t_token	*new;
+	t_token	*aux;
+	char	*tkn;
+
+	tkn = ft_substr(ms->line, s, e - s);
+	printf("DEBUG: Obteniendo el token ft_new_tkn(). Token: #%s#\n", tkn);
+	new = (t_token *) malloc(sizeof(t_token));
+	if (!new)
+		return ;
+	new->type = NOQUOTE;
+	new->token = tkn;
+	//free(tkn);
+	printf("DEBUG: Token: #%s#\n", new->token);
+	new->next = NULL;
+	new->prev = NULL;
+	if (!ms->tokens)
+		ms->tokens = new;
+	else
+	{
+		aux = ms->tokens;
+		while (aux->next)
+			aux = aux->next;
+		new->prev = aux;
+		aux->next = new;
+	}
+}
+
 void	ft_getcmd(t_ms *ms)
 {
 	int	ini;
@@ -54,35 +83,33 @@ void	ft_getcmd(t_ms *ms)
 	printf("DEBUG: En ft_getcmd(), el comando es: %s\n", ms->cmds->cmd);
 }
 
-/*
-void	ft_getargs(t_ms *ms, char *line, int *p)
+void	ft_parse_tokens(t_ms *ms)
 {
-	int		index;
-	int		n_arg;
-	t_list	*arg_node;
+	int	index1;
+	int	index2;
 
-	(*p)++;
-	index = *p;
-	n_arg = 0;
-	ft_printf("DEBUG: Dentro de getargs. Obteniendo posición: %d, contenido: #%c#\n", index, line[index]);
-	while (line[index])
+	index1 = 0;
+	while (ms->line[index1])
 	{
-		while (line[index] && line[index] != ' ')
-			index++;
-		ft_printf("DEBUG: Dentro del bucle de getargs. Obteniendo posición: %d, contenido: #%c#\n", index, line[index]);
-		arg_node = ft_lstnew(ft_substr(line, (*p), index - (*p)));
-		ft_lstadd_back(&ms->cmd->args, arg_node);
-		while (line[index] && line[index] == ' ')
-			index++;
-		*p = index;
+		index2 = index1;
+		if (ms->line[index1] == '\'')
+			ft_simp_quote(ms, &index1, &index2, ms->line[index1]);
+		else if (ms->line[index1] == '\"')
+			ft_dbl_quote(ms, &index1, &index2, ms->line[index1]);
+		else if (ft_strchr(TOKSTR, ms->line[index1]))
+			ft_redir_pipe_node(ms, &index1, &index2, ms->line[index1]);
+		else
+			ft_normal_tok(ms, &index1, &index2);
 	}
-	*p = index;
 }
-*/
+
 void	ft_parser(t_ms *ms)
 {
 	ft_printf("DEBUG: Dentro del parser. La linea es: #%s#\n", ms->line);
-	ft_getcmd(ms);
+	ft_parse_tokens(ms);
+	
+	//ft_getcmd(ms);
+	
 	//ft_printf("DEBUG: El comando es: %s\n", ms->cmds->cmd);
 	//ft_getargs(ms, line, &pos);
 	//ft_printf("DEBUG: El comando tiene %d argumentos\n", ft_lstsize(ms->cmd->args));
