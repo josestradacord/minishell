7c49432d86a97328b68cd7c55bb5a30e2d36f6c7
Jose Estrada Cordero <jose80malaga@gmail.com>
2024-02-24 22:15:57 +0100

    Include colours definitions in minishell.h

--- minishell.h
+++ minishell.h
@@ -49,6 +49,18 @@
 # define HEREDOC	16
 # define PIPE		17
 
+// Colours definitions
+# define BOLD		"\033[1m"
+# define RED		"\033[31;1m"
+# define GREEN		"\033[32;1m"
+# define YELLOW		"\033[33;1m"
+# define CYAN		"\033[36;1m"
+# define BLUE		"\033[34;1m"
+# define BLACK		"\033[30;1m"
+# define MAGENTA	"\033[35;1m"
+# define WHITE		"\033[37;1m"
+# define RESET		"\033[0m"
+
 // Struct to keep the environment variables as a list
 typedef struct s_list_e
 {
